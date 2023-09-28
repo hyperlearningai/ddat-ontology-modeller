a2957e1d0eb0821cc17b16a003d4912a02ec6df3
jillurquddus <jillur.quddus@hyperlearning.ai>
2023-09-28 09:44:16 +0100

    Add numbered list string cleaner

--- ddat/utils/string_utils.py
+++ ddat/utils/string_utils.py
@@ -29,3 +29,17 @@ def pascal_case(text):
 
     pascal_case_str = ''.join(c for c in text.title() if c.isalnum())
     return pascal_case_str[0].upper() + pascal_case_str[1:]
+
+
+def remove_ordered_list_prefix(text):
+    """ Remove the initial number and period prefix from a string.
+
+    Args:
+        text (string): String to transform.
+
+    Returns:
+        String with the initial number and period removed
+
+    """
+
+    return text.lstrip('0123456789. ')
